8312f97a4e664bc653660f916a7a867c3d82a233
Winthrop Gillis <win.gillis@gmail.com>
2014-03-22 18:23:25 -0400

    Push to web server

--- sciencewatch.py
+++ sciencewatch.py
@@ -1,32 +1,18 @@
 from flask import Flask, render_template, session, request, redirect, url_for
-import sqlite3
+import psycopg2, secretKey
 
 app = Flask(__name__) 
-
-def getConn():
-	connection = sqlite3.connect('scienceWatch.db')
-	cursor = connection.cursor()
-	return connection, cursor
 
 
 @app.route('/')
 def index():
-	check = ''
-	if 'username' in session:
-		pass
-	else:
-		check = 'not '
-	return render_template('index.html', var=check)
+	return render_template('index.html')
 
 @app.route('/login', methods=['GET','POST'])
 def login():
 	if request.method == 'POST':
 		user = request.form['user']
 		pwd = request.form['pwd']
-		c, d = getConn()
-		d.execute('insert into users (username, password) values (?,?)', (user, pwd))
-		c.commit()
-		d.close()
 		session['username'] = user
 		return redirect(url_for('index'))
 	else:
@@ -39,7 +25,6 @@ def logout():
 
 
 if __name__=="__main__":
-	app.secret_key = ')\xc1@V\xb6\x8d0K\xc8\x91\xc7\xff\xc4\x96\x91pK4\x9ft\xc7hs\xf5'
+	app.secret_key = secretKey.key
 	app.run(host='127.0.0.1', debug=True)
 
-	
