d8be2e9bff23d728d899500b9c7afafd7c1b391f
Winthrop Gillis <win.gillis@gmail.com>
2014-03-24 01:22:24 -0400

    Working website

--- sciencewatch.py
+++ sciencewatch.py
@@ -1,36 +1,114 @@
 from flask import Flask, render_template, session, request, redirect, url_for
-import secretKey, psycopg2
+import secretKey, time, random, string
+from dbStruct import DbStruct
 
 app = Flask(__name__) 
 
+database = DbStruct()
 
+def generateURL():
+	return ''.join(random.choice(string.ascii_letters + string.digits) for i in range(25))
+
+def signIn(r):
+	user = r.form['user']
+	pwd = r.form['pwd']
+	login = database.checkLogIn(user, pwd)
+	if login != None:
+		session['uuid'] = login
+		return user
+	else:
+		# Username is not in database, so do something about it
+		# Form a response
+		return ''
+
+
+
+
+# Add functionality to being already logged in
 @app.route('/', methods=['GET','POST'])
 def index():
-	if request.method == 'POST':
-		print(request.form['pwd'])
-		# user = request.form['user']
-		# pwd = request.form['pwd']
-		# session['username'] = user
-		return render_template('index.html')
+	if request.method == 'POST':	
+		user = signIn(request)
+		
+		return render_template('index.html', user=True, uname=user)
 
 	else:
-		return render_template('index.html')
+		content = database.getFirst12Articles()
+		rows = []
+		for i in range(3):
+			rows.append([])
+			for j in range(4):	
+				rows[i].append(Entry(content[i*j+j]))
+		t = False
+		info = ''
+		if 'uuid' in session:
+			# generate content
+			# rows variable
+			
+			t = True
+			info = database.getUsername(session['uuid'])
+		return render_template('index.html', user=t, uname = info, rows=rows)
 
 
+# Pretty much all taken care of
 @app.route('/logout')
 def logout():
-	session.pop('username', None)
+	session.pop('uuid', None)
 	return redirect(url_for('index'))
+
+
+@app.route('/postArticle', methods=['GET','POST'])
+def postArticle():
+	if request.method == 'POST':
+		if 'user' in request.form:
+			user = signIn(request)
+			return redirect(url_for('postArticle'))
+		elif 'artUrl' in request.form:
+			c = generateURL()
+			u = database.getUsername(session['uuid'])
+			args = (session['uuid'], u, request.form['artUrl'], time.time(), c, request.form['title'])
+			state = database.addArticle(args)
+			if state:
+				return redirect(url_for('index'))
+			else:
+				return '404'
+	else:
+		signedIn = False
+		u = ''
+		if 'uuid' in session:
+			signedIn = True
+			u = database.getUsername(session['uuid'])
+		return render_template('postArticle.html', signedIn=signedIn, uname=u)
+
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
 	if request.method == 'POST':
-		# Give user an account, add them to database
-		# redirect to home screen
-		return redirect(url_for('index'))
+		user = request.form['user']
+		pwd = request.form['pwd']
+		status = database.addUser(user, pwd)
+		if status:
+			database.execute('select (id) from users where username=%s', (user,))	
+			uuid = database.getOne()[0]
+			session['uuid'] = uuid
+			return redirect(url_for('index'))
+		else:
+			# TODO:
+			# add error message
+			return render_template('register.html')
 	else:
 		# render login form
 		return render_template('register.html')
+
+class Entry:
+	def __init__(self, args):
+		self.articleURL = args[0]
+		self.title = args[2]
+		self.commentURL = args[1]
+
+		
+
 
 if __name__=="__main__":
 	app.secret_key = secretKey.key
